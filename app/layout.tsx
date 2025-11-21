6cb6a4d0efe468539462f70d6dc2ec004ee10f91
VN2006 <91700532+VN2006@users.noreply.github.com>
2025-11-20 23:29:17 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Celer - The World's First End-to-End Multiomics Copilot",
-  description: "Pull, analyze, and visualize complex biological data — all in one intelligent platform.",
+  title: "Celer: Accelerating Multiomics Discovery",
+  description: "Pull, analyze, and visualize complex biological data, all in one intelligent platform.",
   icons: {
     icon: "/c-logo-3d-purple-black-2048.png",
   },
